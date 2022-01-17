9e1b2f3177a7a369281d5abf6c19fa43a783300f
🫒plusbeauxjours <plusbeauxjours@gmail.com>
2022-01-18 00:30:51 +0700

    set: variants

--- src/App.tsx
+++ src/App.tsx
@@ -12,30 +12,60 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 15px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-  _start: { scale: 0 },
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  _start: {
+    opacity: 0,
+    scale: 0.5,
+  },
   _end: {
     scale: 1,
-    rotateZ: 360,
+    opacity: 1,
     transition: {
       type: "spring",
-      delay: 0.5,
-      duration: 2,
-      damping: 8,
-      bounce: 0.8,
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5, // 딜레이 시간(초) 후에 하위 애니메이션이 시작됩니다.
+      staggerChildren: 0.2, // 하위 컴포넌트의 애니메이션에 지속 시간(초)만큼 시차를 둘 수 있습니다.
     },
+  },
+};
+
+const circleVariants = {
+  _start: {
+    opacity: 0,
+    y: 10,
+  },
+  _end: {
+    opacity: 1,
+    y: 0,
   },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="_start" animate="_end" />
+      <Box variants={boxVariants} initial="_start" animate="_end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
