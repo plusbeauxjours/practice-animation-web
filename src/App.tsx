39e58744a84d9a3818c41b85866a4cef5798e3b5
🫒plusbeauxjours <plusbeauxjours@gmail.com>
2022-01-18 18:55:56 +0700

    set: gestures

--- src/App.tsx
+++ src/App.tsx
@@ -12,63 +12,27 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 35px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 const boxVariants = {
-  _start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  _end: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5, // 딜레이 시간(초) 후에 하위 애니메이션이 시작됩니다.
-      staggerChildren: 0.2, // 하위 컴포넌트의 애니메이션에 지속 시간(초)만큼 시차를 둘 수 있습니다.
-    },
-  },
-};
-
-const circleVariants = {
-  _start: {
-    opacity: 0,
-    y: 10,
-  },
-  _end: {
-    opacity: 1,
-    y: 0,
-  },
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "rgb(46, 204, 113)" },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="_start" animate="_end">
-        {/* initial & animate를 넣지 않는 이유는 아래와같이 default로 상속이 되어있기 때문이다. 
-        _start, _end 변수명을 같게, 내용은 다르게해서 실행시키도록 한다. 
-        <Circle variants={circleVariants} initial="_start" animate="_end"/>   */}
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <Box
+        drag
+        variants={boxVariants}
+        whileHover="hover"
+        whileDrag="drag"
+        whileTap="click"
+      />
     </Wrapper>
   );
 }
